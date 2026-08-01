0b6a253df5c1f0d95c6e30e178aa748cbe3b7272
Le Vivilet <levivilet@pm.me>
2026-08-01 22:37:44 +0000

    feature: update eslint dependencies

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,4 @@
+import { defineConfig } from 'eslint/config'
 import * as config from '@lvce-editor/eslint-config'
-import * as actions from '@lvce-editor/eslint-plugin-github-actions'
 
-export default [...config.default, ...actions.default]
+export default defineConfig([...config.default, ...config.recommendedActions])
